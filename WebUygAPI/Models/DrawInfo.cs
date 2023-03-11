fb7ebe8bfb4db7472681ceb0c92ce34266e4e013
Onur Bozkurt <onurbozkurt166@gmail.com>
2023-03-11 21:00:18 +0300

    Validation to be added

--- WebUygAPI/Models/DrawInfo.cs
+++ WebUygAPI/Models/DrawInfo.cs
@@ -5,7 +5,7 @@
 
     {
         public int Id { get; set; }
-        public string username { get; set; }
+        public string Name { get; set; }
         public string number { get; set; }
         public object coordinates { get; set; }
 
