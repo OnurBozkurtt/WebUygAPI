358bf5aedd30a89427e58cd17e5d9ce4346d4186
OnurBozkurtt <104000051+OnurBozkurtt@users.noreply.github.com>
2023-03-19 03:33:43 +0300

    Update DrawInfo.cs

--- WebUygAPI/Models/DrawInfo.cs
+++ WebUygAPI/Models/DrawInfo.cs
@@ -1,11 +1,18 @@
-﻿namespace WebUygAPI.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUygAPI.Models
  
 {
     public class DrawInfo
 
     {
+        
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required(ErrorMessage = "You must provide a Name.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Invalid character")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "You must provide a Number.")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Invalid number")]
         public string number { get; set; }
         public object coordinates { get; set; }
 
